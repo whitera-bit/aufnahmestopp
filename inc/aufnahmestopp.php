f790536a582fd4811e5dbc961f885d454e39751a
Tom <90357560+whitera-bit@users.noreply.github.com>
2023-07-25 14:20:46 +0200

    Update aufnahmestopp.php

--- inc/aufnahmestopp.php
+++ inc/aufnahmestopp.php
@@ -53,7 +53,7 @@ function aufnahmestopp_install()
     
     // Tabellenerweiterung der users-Tabelle für die Index Nachricht
 
-    $db->query("ALTER TABLE `".TABLE_PREFIX."users` ADD `noticeboard_new` int(11) NOT NULL DEFAULT '0';");
+    $db->query("ALTER TABLE `".TABLE_PREFIX."users` ADD `aufnahmestopp_new` int(11) NOT NULL DEFAULT '0';");
     
 
     // Einstellungen ACP
@@ -161,7 +161,7 @@ rebuild_settings();
     find_replace_templatesets('header', '#'.preg_quote('{$bbclosedwarning}').'#', '{$aufnahmestopp_new} {$bbclosedwarning}');
 }
 
-function noticeboard_deactivate()
+function aufnahmestopp_deactivate()
 {
      global $db, $cache;
 
@@ -177,9 +177,127 @@ function noticeboard_deactivate()
     
     $plugins->add_hook('global_start', 'aufnahmestopp_global');
 
-    function noticeboard_global(){
+    function aufnahmestopp_global(){
 
+	global $db, $mybb, $templates, $aufnahmestopp_new, $new_aufnahmestopp, $aufnahmestopp_read, $lang;
 
+	 // Information zu neuem Aufnahmestopp auf dem Index
+	    
+	 if(is_member($mybb->settings['aufnahmestopp'])) {
+	
+	        $lang->load('aufnahmestopp');
+	
+	        $uid = $mybb->user['uid'];
+	
+	        $aufnahmestopp_read = "<a href='misc.php?action=aufnahmestopp_read&read={$uid}' original-title='als gelesen markieren'><i class=\"fas fa-trash\" style=\"float: right;font-size: 14px;padding: 1px;\"></i></a>";
+	
+		 // User hat Info auf dem Index gelesen
+		
+		            if ($mybb->get_input ('action') == 'aufnahmestopp_read') {
+		
+		                $this_user = intval ($mybb->user['uid']);
+		
+		                $as_uid = intval ($mybb->user['as_uid']);
+		                $read = $mybb->input['read'];
+		                if ($read) {
+		                    if($as_uid == 0){
+		                        $db->query ("UPDATE ".TABLE_PREFIX."users SET aufnahmestopp_new = 1  WHERE (as_uid = $this_user) OR (uid = $this_user)");
+		                    }elseif ($as_uid != 0){
+		                        $db->query ("UPDATE ".TABLE_PREFIX."users SET aufnahmestopp_new = 1  WHERE (as_uid = $as_uid) OR (uid = $this_user) OR (uid = $as_uid)");
+		                    }
+		                    redirect("index.php");
+		                }
+		            }
+		    }
+		
+		    $select = $db->query ("SELECT * FROM " . TABLE_PREFIX ."aufnahmestopp WHERE visible = 1");
+		    $row_cnt = mysqli_num_rows ($select);
+		    if ($row_cnt > 0) {
+		        $select = $db->query ("SELECT aufnahmestopp_new FROM " . TABLE_PREFIX . "users 
+		        WHERE uid = '" . $mybb->user['uid'] . "' LIMIT 1");
+		
+		
+		        $data = $db->fetch_array ($select);
+		        if (isset($data['aufnahmestopp_new']) == '0') {
+		
+		            eval("\$new_aufnahmestopp = \"" . $templates->get ("aufnahmestopp_alert") . "\";");
+		
+		        }
+		            
+		    }
+		
+		}
+		
+		
+		// WER IST ONLINE Anzeige
+		
+		
+		$plugins->add_hook("fetch_wol_activity_end", "aufnahmestopp_online_activity");
+		$plugins->add_hook("build_friendly_wol_location_end", "aufnahmestopp_online_location");
+		
+		function aufnahmestopp_online_activity($user_activity) {
+		global $parameters;
+		
+		    $split_loc = explode(".php", $user_activity['location']);
+		    if($split_loc[0] == $user['location']) {
+		        $filename = '';
+		    } else {
+		        $filename = my_substr($split_loc[0], -my_strpos(strrev($split_loc[0]), "/"));
+		    }
+		    
+		    switch ($filename) {
+		        case 'aufnahmestopp':
+		        if($parameters['action'] == "" && empty($parameters['site'])) {
+		            $user_activity['activity'] = "aufnahmestopp";
+		        }
+		        if($parameters['action'] == "overview" && empty($parameters['site'])) {
+		            $user_activity['activity'] = "overview";
+		        }
+		        if($parameters['action'] == "free" && empty($parameters['site'])) {
+		            $user_activity['activity'] = "free";
+		        }
+		        if($parameters['action'] == "taken" && empty($parameters['site'])) {
+		            $user_activity['activity'] = "taken";
+		        }
+		        if($parameters['action'] == "add" && empty($parameters['site'])) {
+		            $user_activity['activity'] = "add";
+		        }
+		        if($parameters['action'] == "edit" && empty($parameters['site'])) {
+		            $user_activity['activity'] = "edit";
+		        }
+		        break;
+		    }
+		      
+		return $user_activity;
+		}
+		
+		function aufnahmestopp_online_location($plugin_array) {
+		global $mybb, $theme, $lang;
+		
+		    if($plugin_array['user_activity']['activity'] == "aufnahmestopp") {
+		        $plugin_array['location_name'] = "Betrachtet die <a href=\"aufnahmestopp.php\">Anschlagstafel</a>.";
+		    }
+			if($plugin_array['user_activity']['activity'] == "overview") {
+				$plugin_array['location_name'] = "Studiert die <a href=\"aufnahmestopp.php?action=overview\">Aufträge</a>.";
+			}
+		    if($plugin_array['user_activity']['activity'] == "free") {
+				$plugin_array['location_name'] = "Sieht sich freie <a href=\"aufnahmestopp.php?action=free\">Aufträge</a> an.";
+			}
+		    if($plugin_array['user_activity']['activity'] == "taken") {
+				$plugin_array['location_name'] = "Sieht sich vergebene <a href=\"aufnahmestopp.php?action=taken\">Aufträge</a> an.";
+			}
+		    if($plugin_array['user_activity']['activity'] == "finished") {
+				$plugin_array['location_name'] = "Sieht sich erledigte <a href=\"aufnahmestopp.php?action=finished\">Aufträge</a> an.";
+			}
+		    if($plugin_array['user_activity']['activity'] == "add") {
+				$plugin_array['location_name'] = "Pinnt einen neuen Auftrag an.";
+			}
+		    if($plugin_array['user_activity']['activity'] == "edit") {
+				$plugin_array['location_name'] = "Bessert Fehler auf einem Auftrag aus.";
+			}
+		
+		return $plugin_array;
+		}
 
 
 
